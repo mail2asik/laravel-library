06c268f7d98125d47783128a58e21f703478b8b0
Mohamed Asik <Asik@Mohameds-MacBook-Pro.local>
2016-10-30 16:33:57 +0800

    Fixed that router not catchup the controller due to case sensitive (in prod env)

--- app/Http/Routes/Admin.php
+++ app/Http/Routes/Admin.php
@@ -21,30 +21,30 @@ Route::group(['prefix' => 'admin', 'middleware' => 'adminAuth'], function () {
     });
 
     // Users
-    Route::get('users', ['as' => 'admin.users', 'uses' => 'admin\UserController@index']);
-    Route::get('/users/create', ['as' => 'admin.users.create', 'uses' => 'admin\UserController@create']);
+    Route::get('users', ['as' => 'admin.users', 'uses' => 'Admin\UserController@index']);
+    Route::get('/users/create', ['as' => 'admin.users.create', 'uses' => 'Admin\UserController@create']);
     Route::group(array('middleware' => 'csrf'), function () {
-        Route::post('doCreateUser', ['as' => 'admin.postCreateUser', 'uses' => 'admin\UserController@doCreateUser']);
+        Route::post('doCreateUser', ['as' => 'admin.postCreateUser', 'uses' => 'Admin\UserController@doCreateUser']);
     });
-    Route::get('/users/update/{user_uid}', ['as' => 'admin.users.update', 'uses' => 'admin\UserController@update']);
+    Route::get('/users/update/{user_uid}', ['as' => 'admin.users.update', 'uses' => 'Admin\UserController@update']);
     Route::group(array('middleware' => 'csrf'), function () {
-        Route::post('doUpdateUser/{user_uid}', ['as' => 'admin.postUpdateUser', 'uses' => 'admin\UserController@doUpdateUser']);
+        Route::post('doUpdateUser/{user_uid}', ['as' => 'admin.postUpdateUser', 'uses' => 'Admin\UserController@doUpdateUser']);
     });
-    Route::get('/users/delete/{user_uid}', ['as' => 'admin.users.delete', 'uses' => 'admin\UserController@delete']);
+    Route::get('/users/delete/{user_uid}', ['as' => 'admin.users.delete', 'uses' => 'Admin\UserController@delete']);
 
     // Books
-    Route::get('books', ['as' => 'admin.books', 'uses' => 'admin\BookController@index']);
-    Route::get('/books/create', ['as' => 'admin.books.create', 'uses' => 'admin\BookController@create']);
+    Route::get('books', ['as' => 'admin.books', 'uses' => 'Admin\BookController@index']);
+    Route::get('/books/create', ['as' => 'admin.books.create', 'uses' => 'Admin\BookController@create']);
     Route::group(array('middleware' => 'csrf'), function () {
-        Route::post('doCreateBook', ['as' => 'admin.postCreateBook', 'uses' => 'admin\BookController@doCreateBook']);
+        Route::post('doCreateBook', ['as' => 'admin.postCreateBook', 'uses' => 'Admin\BookController@doCreateBook']);
     });
-    Route::get('/books/update/{book_uid}', ['as' => 'admin.books.update', 'uses' => 'admin\BookController@update']);
+    Route::get('/books/update/{book_uid}', ['as' => 'admin.books.update', 'uses' => 'Admin\BookController@update']);
     Route::group(array('middleware' => 'csrf'), function () {
-        Route::post('doUpdateBook/{book_uid}', ['as' => 'admin.postUpdateBook', 'uses' => 'admin\BookController@doUpdateBook']);
+        Route::post('doUpdateBook/{book_uid}', ['as' => 'admin.postUpdateBook', 'uses' => 'Admin\BookController@doUpdateBook']);
     });
-    Route::get('/books/delete/{book_uid}', ['as' => 'admin.books.delete', 'uses' => 'admin\BookController@delete']);
-    Route::get('/books/collect/{book_uid}/{user_uid}', ['as' => 'admin.books.collectBook', 'uses' => 'admin\BookController@collectBook']);
+    Route::get('/books/delete/{book_uid}', ['as' => 'admin.books.delete', 'uses' => 'Admin\BookController@delete']);
+    Route::get('/books/collect/{book_uid}/{user_uid}', ['as' => 'admin.books.collectBook', 'uses' => 'Admin\BookController@collectBook']);
 
     // Reports
-    Route::get('reports', ['as' => 'admin.reports', 'uses' => 'admin\ReportController@index']);
+    Route::get('reports', ['as' => 'admin.reports', 'uses' => 'Admin\ReportController@index']);
 });
